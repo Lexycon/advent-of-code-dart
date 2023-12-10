25437c3d0d298b8df328b38d087f82da187b8017
Lexycon <berchtenbreiterd@gmail.com>
2023-12-10 22:00:55 +0100

    Added util methods for math and vec2 support.

--- lib/src/utils/math.dart
+++ lib/src/utils/math.dart
@@ -1,2 +1,21 @@
+import 'package:advent_of_code_dart/src/utils/vec2.dart';
+
 int lcm(int a, int b) => (a * b) ~/ a.gcd(b);
 int gcd(int a, int b) => a.gcd(b);
+
+double cross(double x1, double y1, double x2, double y2) {
+  return x1 * y2 - x2 * y1;
+}
+
+double polygonArea(List<Vec2> points) {
+  double a = 0.0;
+  for (int i = 2; i < points.length; i++) {
+    a += cross(points[i].x - points[0].x, points[i].y - points[0].y,
+        points[i - 1].x - points[0].x, points[i - 1].y - points[0].y);
+  }
+  return (a / 2).abs();
+}
+
+double polygonPointsInside(double area, int pointsBoundary) {
+  return area + 1 - pointsBoundary / 2;
+}
---
+++ lib/src/utils/vec2.dart
@@ -0,0 +1,76 @@
+class Vec2 {
+  const Vec2(this.x, this.y);
+
+  Vec2.int(int x, int y)
+      : x = x.toDouble(),
+        y = y.toDouble();
+
+  final double x;
+  final double y;
+
+  int get xInt => x.toInt();
+  int get yInt => y.toInt();
+
+  static const Vec2 zero = Vec2(0, 0);
+  static const Vec2 up = Vec2(0, -1);
+  static const Vec2 upRight = Vec2(1, -1);
+  static const Vec2 right = Vec2(1, 0);
+  static const Vec2 downRight = Vec2(1, 1);
+  static const Vec2 down = Vec2(0, 1);
+  static const Vec2 downLeft = Vec2(-1, 1);
+  static const Vec2 left = Vec2(-1, 0);
+  static const Vec2 upLeft = Vec2(-1, -1);
+
+  static const List<Vec2> cardinalDirs = <Vec2>[
+    Vec2.upLeft,
+    Vec2.up,
+    Vec2.upRight,
+    Vec2.left,
+    Vec2.zero,
+    Vec2.right,
+    Vec2.downLeft,
+    Vec2.down,
+    Vec2.downRight,
+  ];
+
+  static const List<Vec2> aroundDirs = <Vec2>[
+    Vec2.upLeft,
+    Vec2.up,
+    Vec2.upRight,
+    Vec2.left,
+    Vec2.right,
+    Vec2.downLeft,
+    Vec2.down,
+    Vec2.downRight,
+  ];
+
+  Vec2 operator +(Vec2 other) => Vec2(x + other.x, y + other.y);
+
+  Vec2 operator -() => Vec2(-x, -y);
+
+  Vec2 operator -(Vec2 other) => Vec2(x - other.x, y - other.y);
+
+  Vec2 operator *(num factor) => Vec2(x * factor, y * factor);
+
+  static Iterable<Vec2> range(Vec2 min, Vec2 max) sync* {
+    for (int y = min.yInt; y < max.yInt; y++) {
+      for (int x = min.xInt; x < max.xInt; x++) {
+        yield Vec2.int(x, y);
+      }
+    }
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(other, this) ||
+      (other is Vec2 &&
+          other.runtimeType == runtimeType &&
+          other.x == x &&
+          other.y == y);
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode;
+
+  @override
+  String toString() => 'Vec2($x, $y)';
+}
